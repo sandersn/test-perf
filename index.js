95543fd420f501cb4999517def3c23f9fff395ec
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2019-06-07 15:33:09 -0700

    Retrieval of files for commits is working

--- index.js
+++ index.js
@@ -1,18 +1,53 @@
 const assert = require('assert')
 const fs = require('fs')
 const tsPath = "../../ts"
+const git = require('simple-git/promise')(tsPath)
 assert(fs.existsSync(tsPath), "Can't find Typescript at " + tsPath)
-// 1. find all merge commits in the last X time span from present: git log --merges
-//   2. find all commits for each merge commit: git log c^..c (+something to only return commit shas)
-//   3. concatMap all the files from each commit: git log --stat
-// 2. find all merge-squash commits in the last X time span: git log | egrep '\(\#[0-9]+\)$'
-//   1. find all the files from each commit
-// 3. create mapping from src/compiler/* to various test suites
-for (const d of fs.readdirSync(tsPath)) {
-    console.log('Running prettier on', d)
-    // Actually run prettier
-    // Add to list if prettier caused a diff and if there is no open PR that covers this directory
-    // Stop when the list has 10 elements
-    // Then create a PR
-    break
+async function main() {
+    const commits = await git.log({ from: "f617d1641b00366ff9c5684fec06cec7f1c26d2e", to: "master" })
+    /** @type {Map<string, Set<string>>} */
+    const map = new Map()
+    for (const commit of commits.all) {
+        if (isMergeCommit(commit.message) || isSquashMergeMessage(commit.message)) {
+            const files = parseFiles(await git.show([commit.hash, "--stat=1000,960,40", "--pretty=oneline"]))
+            fillMap(files, map)
+        }
+    }
 }
+
+/**
+ * @param {string} s
+ */
+function isSquashMergeMessage(s) {
+    return /\(#[0-9]+\)$/.test(s)
+}
+
+/**
+ * @param {string} s
+ */
+function isMergeCommit(s) {
+    return /Merge pull request #[0-9]+/.test(s)
+}
+
+/**
+ * @param {string} s
+ */
+function parseFiles(s) {
+    const lines = s.split('\n')
+    // Note that slice(2) only works for merge commits, which have an empty newline after the title
+    return new Set(lines.slice(2, lines.length - 2).map(line => line.split("|")[0].trim()))
+}
+
+/**
+ * @param {Set<string>} files
+ * @param {Map<string, Set<string>>} map
+ */
+function fillMap(files, map) {
+    for (const f of files) {
+        if (/^src\/compiler/.test(f)) {
+
+        }
+    }
+}
+
+main().catch(e => { console.log(e); process.exit(1) })
